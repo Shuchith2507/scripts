854f139cae89230b19d96ba00a435846e53834c0
Shuchith2507 <shuchithbu@gmail.com>
2024-02-05 09:08:04 +0000

    first

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 git clone https://github.com/Shuchith2507/sampleapp.git
 echo "Copy"
-cd ..
+
 mv sampleapp sample
-sudo -s
-cp -Rf sample /opt/tomcat/webapps
-pwd
+cd sample
+ls -a
+#sudo -s
+
+#cp -Rf sample /opt/tomcat/webapps
+#pwd
 #ls -a
 
 echo "Copy DOne"
